8c25ee0b5782bf12cac6e32307ff1571f99df08d
programmerprime <tmai.prime@gmail.com>
2020-09-12 19:04:50 +0600

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCEnSZ1syQMDDXIcLUjN1wx_NLrwDDLOYA",
+    authDomain: "whatsapp-clone-prime.firebaseapp.com",
+    databaseURL: "https://whatsapp-clone-prime.firebaseio.com",
+    projectId: "whatsapp-clone-prime",
+    storageBucket: "whatsapp-clone-prime.appspot.com",
+    messagingSenderId: "110036387790",
+    appId: "1:110036387790:web:7b1d189ffe13b8809d8d9f"
+  };
+
+
+  export default firebaseConfig;
